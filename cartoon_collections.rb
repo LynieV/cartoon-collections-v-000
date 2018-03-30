ab6950fbf45516a51f9c2f9497190d4ad9974a16
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-30 15:42:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  
+  if calls_long.include? 
+    true
+  else
+    false
 end
 
 def find_the_cheese# code an argument here
