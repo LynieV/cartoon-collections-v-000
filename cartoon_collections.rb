89bd540866cfa69abc5204fe09bb8a183e01d61b
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-30 17:07:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if food.include?("cheddar", "gouda", "camembert")
+  if food.include?(cheese_types)
     true
   else
     nil
