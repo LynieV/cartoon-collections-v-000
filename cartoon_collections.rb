e5ebcc0c2c226f161484db22720528fe0b363458
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-30 16:51:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if food.find == cheese_types
+  if food.include? == cheese_types
   else
     nil
   end
