8f2e172b7a296fca8b48afa26d1a44818fba57b7
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-30 16:47:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,11 @@ def long_planeteer_calls(calls_long)
   end
 end
 
-def find_the_cheese(cheese_types)
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.find
+  if cheese.find == cheese_types
+    true
   else
     nil
   end
